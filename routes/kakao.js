a91fc27b328d7c0e9dc26f026928f63411cad631
JohnJeong123 <frenchofkiss@gmail.com>
2019-02-27 15:01:57 +0900

    added SendATS buttonList param

--- routes/kakao.js
+++ routes/kakao.js
@@ -159,13 +159,18 @@ router.get('/sendATS_one', function (req, res, next) {
 
     // 알림톡 템플릿코드
     // 승인된 알림톡 템플릿 코드는 ListATStemplate API, GetATSTemplateMgtURL API, 혹은 팝빌사이트에서 확인이 가능합니다.
-    var templateCode = '018110000047';
+    var templateCode = '019020000163';
 
     // 발신번호 (팝빌에 등록된 발신번호만 이용가능)
     var snd = '070-4304-2992';
 
     // 알림톡 내용 (최대 1000자)
-    var content = '테스트 템플릿 입니다.';
+    var content = '[ 팝빌 ]\n';
+    content += '신청하신 #{템플릿코드}에 대한 심사가 완료되어 승인 처리되었습니다.\n';
+    content += '해당 템플릿으로 전송 가능합니다.\n\n';
+    content += '문의사항 있으시면 파트너센터로 편하게 연락주시기 바랍니다.\n\n';
+    content += '팝빌 파트너센터 : 1600-8536\n';
+    content += 'support@linkhub.co.kr';
 
     // 대체문자 내용 (최대 2000byte)
     var altContent = '알림톡 대체 문자';
@@ -190,7 +195,20 @@ router.get('/sendATS_one', function (req, res, next) {
     // 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
     var requestNum = "";
 
-    kakaoService.sendATS_one(testCorpNum, templateCode, snd, content, altContent, altSendType, sndDT, receiver, receiverName, UserID, requestNum,
+    // 알림톡 버튼정보를 템플릿 신청시 기재한 버튼정보와 동일하게 전송하는 경우 btns를 null 처리.
+    var btns = null;
+
+    // 알림톡 버튼 URL에 #{템플릿변수}를 기재한경우 템플릿변수 영역을 변경하여 버튼정보 구성
+    // var btns = [
+    //     {
+    //         n: '템플릿 안내',               //버튼명
+    //         t: 'WL',                      //버튼유형 [WL-웹링크, AL-앱링크, MD-메시지전달, BK-봇키워드]
+    //         u1: 'https://www.popbill.com', //[앱링크-Android, 웹링크-Mobile]
+    //         u2: 'http://www.popbill.com'  //[앱링크-IOS, 웹링크-PC URL]
+    //     }
+    // ];
+
+    kakaoService.sendATS_one(testCorpNum, templateCode, snd, content, altContent, altSendType, sndDT, receiver, receiverName, UserID, requestNum, btns,
         function (receiptNum) {
             res.render('Kakao/receiptNum', {path: req.path, receiptNum: receiptNum});
         }, function (Error) {
@@ -209,7 +227,15 @@ router.get('/sendATS_multi', function (req, res, next) {
 
     // 알림톡 템플릿코드
     // 승인된 알림톡 템플릿 코드는 ListATStemplate API, GetATSTemplateMgtURL, 혹은 팝빌사이트에서 확인이 가능합니다.
-    var templateCode = '018110000047';
+    var templateCode = '019020000163';
+
+    // 알림톡 내용 (최대 1000자)
+    var content = '[ 팝빌 ]\n';
+    content += '신청하신 #{템플릿코드}에 대한 심사가 완료되어 승인 처리되었습니다.\n';
+    content += '해당 템플릿으로 전송 가능합니다.\n\n';
+    content += '문의사항 있으시면 파트너센터로 편하게 연락주시기 바랍니다.\n\n';
+    content += '팝빌 파트너센터 : 1600-8536\n';
+    content += 'support@linkhub.co.kr';
 
     // 발신번호 (팝빌에 등록된 발신번호만 이용가능)
     var snd = '070-4304-2992';
@@ -225,13 +251,13 @@ router.get('/sendATS_multi', function (req, res, next) {
         {
             rcv: '010111222',           //수신번호
             rcvnm: 'popbill',           //수신자명
-            msg: '테스트 템플릿 입니다.',    //알림톡 내용
+            msg: content,    //알림톡 내용
             altmsg: '알림톡 대체 문자_0'    //대체문자 내용
         },
         {
             rcv: '010111222',
             rcvnm: 'linkhub',
-            msg: '테스트 템플릿 입니다1',
+            msg: content,
             altmsg: '알림톡 대체 문자_1'
         }
     ];
@@ -244,7 +270,20 @@ router.get('/sendATS_multi', function (req, res, next) {
     // 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
     var requestNum = "";
 
-    kakaoService.sendATS_multi(testCorpNum, templateCode, snd, altSendType, sndDT, msgs, UserID, requestNum,
+    // 알림톡 버튼정보를 템플릿 신청시 기재한 버튼정보와 동일하게 전송하는 경우 btns를 null 처리.
+    var btns = null;
+
+    // 알림톡 버튼 URL에 #{템플릿변수}를 기재한경우 템플릿변수 영역을 변경하여 버튼정보 구성
+    // var btns = [
+    //     {
+    //         n: '템플릿 안내',               //버튼명
+    //         t: 'WL',                      //버튼유형 [WL-웹링크, AL-앱링크, MD-메시지전달, BK-봇키워드]
+    //         u1: 'https://www.popbill.com', //[앱링크-Android, 웹링크-Mobile]
+    //         u2: 'http://www.popbill.com'  //[앱링크-IOS, 웹링크-PC URL]
+    //     }
+    // ];
+
+    kakaoService.sendATS_multi(testCorpNum, templateCode, snd, altSendType, sndDT, msgs, UserID, requestNum, btns,
         function (receiptNum) {
             res.render('Kakao/receiptNum', {path: req.path, receiptNum: receiptNum});
         }, function (Error) {
@@ -263,7 +302,15 @@ router.get('/sendATS_same', function (req, res, next) {
 
     // 알림톡 템플릿코드
     // 승인된 알림톡 템플릿 코드는 ListATStemplate API, GetATSTemplateMgtURL API, 혹은 팝빌사이트에서 확인이 가능합니다.
-    var templateCode = '018110000047';
+    var templateCode = '019020000163';
+
+    // 알림톡 내용 (최대 1000자)
+    var content = '[ 팝빌 ]\n';
+    content += '신청하신 #{템플릿코드}에 대한 심사가 완료되어 승인 처리되었습니다.\n';
+    content += '해당 템플릿으로 전송 가능합니다.\n\n';
+    content += '문의사항 있으시면 파트너센터로 편하게 연락주시기 바랍니다.\n\n';
+    content += '팝빌 파트너센터 : 1600-8536\n';
+    content += 'support@linkhub.co.kr';
 
     // 발신번호 (팝빌에 등록된 발신번호만 이용가능)
     var snd = '070-4304-2992';
@@ -300,7 +347,20 @@ router.get('/sendATS_same', function (req, res, next) {
     // 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
     var requestNum = "";
 
-    kakaoService.sendATS_same(testCorpNum, templateCode, snd, content, altContent, altSendType, sndDT, msgs, UserID, requestNum,
+    // 알림톡 버튼정보를 템플릿 신청시 기재한 버튼정보와 동일하게 전송하는 경우 btns를 null 처리.
+    var btns = null;
+
+    // 알림톡 버튼 URL에 #{템플릿변수}를 기재한경우 템플릿변수 영역을 변경하여 버튼정보 구성
+    // var btns = [
+    //     {
+    //         n: '템플릿 안내',               //버튼명
+    //         t: 'WL',                      //버튼유형 [WL-웹링크, AL-앱링크, MD-메시지전달, BK-봇키워드]
+    //         u1: 'https://www.popbill.com', //[앱링크-Android, 웹링크-Mobile]
+    //         u2: 'http://www.popbill.com'  //[앱링크-IOS, 웹링크-PC URL]
+    //     }
+    // ];
+
+    kakaoService.sendATS_same(testCorpNum, templateCode, snd, content, altContent, altSendType, sndDT, msgs, UserID, requestNum, btns,
         function (receiptNum) {
             res.render('Kakao/receiptNum', {path: req.path, receiptNum: receiptNum});
         }, function (Error) {
