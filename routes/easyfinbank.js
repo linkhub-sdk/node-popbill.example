499ba52fc7e0487b585d69bca95ee32d550eac5e
JohnJeong123 <frenchofkiss@gmail.com>
2020-06-02 17:40:31 +0900

    added EasyFinBankService API

--- routes/easyfinbank.js
+++ routes/easyfinbank.js
@@ -55,6 +55,196 @@ router.get('/getBankAccountMgtURL', function (req, res, next) {
         });
 });
 
+
+/*
+ * 계좌를 등록합니다.
+ */
+router.get('/registBankAccount', function (req, res, next) {
+
+    // 팝빌회원 사업자번호, '-' 제외 10자리
+    var testCorpNum = '1234567890';
+
+    // 계좌정보
+    var bankAccountInfo =  {
+
+      // [필수] 은행코드
+      // 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
+      // SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
+      // 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
+      BankCode : '',
+
+      // [필수] 계좌번호, 하이픈('-') 제외
+      AccountNumber : '',
+
+      // [필수] 계좌비밀번호
+      AccountPWD : '',
+
+      // [필수] 계좌유형, "법인" 또는 "개인" 입력
+      AccountType : '',
+
+      // [필수] 예금주 식별정보 (‘-‘ 제외)
+      // 계좌유형이 “법인”인 경우 : 사업자번호(10자리)
+      // 계좌유형이 “개인”인 경우 : 예금주 생년월일 (6자리-YYMMDD)
+      IdentityNumber : '',
+
+      // 계좌 별칭
+      AccountName : '',
+
+      // 인터넷뱅킹 아이디 (국민은행 필수)
+      BankID : '',
+
+      // 조회전용 계정 아이디 (대구은행, 신협, 신한은행 필수)
+      FastID : '',
+
+      // 조회전용 계정 비밀번호 (대구은행, 신협, 신한은행 필수
+      FastPWD : '',
+
+      // 결제기간(개월), 1~12 입력가능, 미기재시 기본값(1) 처리
+      // - 파트너 과금방식의 경우 입력값에 관계없이 1개월 처리
+      UsePeriod : '1',
+
+      // 메모
+      Memo : ''
+    };
+
+    easyFinBankService.registBankAccount(testCorpNum, bankAccountInfo,
+        function (result) {
+            res.render('response', {path: req.path, code: result.code, message: result.message});
+        }, function (Error) {
+            res.render('response', {path: req.path, code: Error.code, message: Error.message});
+        });
+});
+
+/*
+ * 팝빌에 등록된 계좌정보를 수정합니다.
+ */
+router.get('/updateBankAccount', function (req, res, next) {
+
+    // 팝빌회원 사업자번호, '-' 제외 10자리
+    var testCorpNum = '1234567890';
+
+    // 계좌정보
+    var bankAccountInfo =  {
+
+      // [필수] 은행코드
+      // 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
+      // SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
+      // 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
+      BankCode : '',
+
+      // [필수] 계좌번호, 하이픈('-') 제외
+      AccountNumber : '',
+
+      // [필수] 계좌비밀번호
+      AccountPWD : '',
+
+      // 계좌 별칭
+      AccountName : '',
+
+      // 인터넷뱅킹 아이디 (국민은행 필수)
+      BankID : '',
+
+      // 조회전용 계정 아이디 (대구은행, 신협, 신한은행 필수)
+      FastID : '',
+
+      // 조회전용 계정 비밀번호 (대구은행, 신협, 신한은행 필수
+      FastPWD : '',
+
+      // 메모
+      Memo : ''
+    };
+
+    easyFinBankService.updateBankAccount(testCorpNum, bankAccountInfo,
+        function (result) {
+            res.render('response', {path: req.path, code: result.code, message: result.message});
+        }, function (Error) {
+            res.render('response', {path: req.path, code: Error.code, message: Error.message});
+        });
+});
+
+/*
+* 팝빌에 등록된 은행계좌의 정액제 해지를 요청한다.
+*/
+router.get('/closeBankAccount', function (req, res, next) {
+
+    // 팝빌회원 사업자번호, '-' 제외 10자리
+    var testCorpNum = '1234567890';
+
+    // 은행코드
+    // 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
+    // SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
+    // 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
+    var bankCode = "";
+
+    // 계좌번호, 하이픈('-') 제외
+    var accountNumber = "";
+
+    // 해지유형, "일반", "중도" 중 선택기재
+    // 일반해지 - 이용중인 정액제 사용기간까지 이용후 정지
+    // 중도해지 - 요청일 기준으로 정지, 정액제 잔여기간은 일할로 계산되어 포인트 환불
+    var closeType = "중도"
+
+    easyFinBankService.closeBankAccount(testCorpNum, bankCode, accountNumber, closeType,
+        function (result) {
+            res.render('response', {path: req.path, code: result.code, message: result.message});
+        }, function (Error) {
+            res.render('response', {path: req.path, code: Error.code, message: Error.message});
+        });
+});
+
+/*
+* 계좌의 정액제 해지신청을 취소합니다.
+*/
+router.get('/revokeCloseBankAccount', function (req, res, next) {
+
+    // 팝빌회원 사업자번호, '-' 제외 10자리
+    var testCorpNum = '1234567890';
+
+    // 은행코드
+    // 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
+    // SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
+    // 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
+    var bankCode = "";
+
+    // 계좌번호, 하이픈('-') 제외
+    var accountNumber = "";
+
+    easyFinBankService.revokeCloseBankAccount(testCorpNum, bankCode, accountNumber,
+        function (result) {
+            res.render('response', {path: req.path, code: result.code, message: result.message});
+        }, function (Error) {
+            res.render('response', {path: req.path, code: Error.code, message: Error.message});
+        });
+});
+
+
+
+/*
+ * 계좌정보를 확인합니다.
+ */
+router.get('/getBankAccountInfo', function (req, res, next) {
+
+    // 팝빌회원 사업자번호, '-' 제외 10자리
+    var testCorpNum = '1234567890';
+
+    // 은행코드
+    // 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
+    // SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
+    // 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
+    var bankCode = "";
+
+    // 계좌번호, 하이픈('-') 제외
+    var accountNumber = "";
+
+    easyFinBankService.getBankAccountInfo(testCorpNum, bankCode, accountNumber,
+        function (result) {
+            res.render('EasyFinBank/getBankAccountInfo', {path: req.path, info: result});
+        }, function (Error) {
+            res.render('response', {path: req.path, code: Error.code, message: Error.message});
+        });
+});
+
+
 /*
  * 팝빌에 등록된 은행 계좌 목록을 확인합니다.
  * - https://docs.popbill.com/easyfinbank/node/api#ListBankAccount
@@ -85,7 +275,7 @@ router.get('/requestJob', function (req, res, next) {
     // 은행코드
     var bankCode = '0048';
 
-    // 계좌번호
+    // 계좌번호,  하이픈('-') 제외
     var accountNumber = '131020538645';
 
     // 시작일자, 날짜형식(yyyyMMdd)
@@ -260,7 +450,7 @@ router.get('/getFlatRateState', function (req, res, next) {
     // 은행코드
     var bankCode = '0048';
 
-    // 계좌번호
+    // 계좌번호, 하이픈('-') 제외
     var accountNumber = '131020538645';
 
     easyFinBankService.getFlatRateState(testCorpNum, bankCode, accountNumber,
