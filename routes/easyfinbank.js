d4aa9bdb17c1e7cc7fd49c4daa2627b4d11d2ab4
1jae <wjkim@linkhub.co.kr>
2020-11-24 10:31:11 +0900

    added deleteBankAccount

--- routes/easyfinbank.js
+++ routes/easyfinbank.js
@@ -223,7 +223,31 @@ router.get('/revokeCloseBankAccount', function (req, res, next) {
         });
 });
 
+/*
+* 팝빌에 등록된 은행계좌를 삭제 합니다.
+* 종량제를 이용시 사용, 정액제 이용시 사용 불가.
+*/
+router.get('/deleteBankAccount', function (req, res, next) {
 
+    // 팝빌회원 사업자번호, '-' 제외 10자리
+    var testCorpNum = '1234567890';
+
+    // 은행코드
+    // 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
+    // SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
+    // 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
+    var bankCode = "";
+
+    // 계좌번호, 하이픈('-') 제외
+    var accountNumber = "";
+
+    easyFinBankService.deleteBankAccount(testCorpNum, bankCode, accountNumber,
+        function (result) {
+            res.render('response', {path: req.path, code: result.code, message: result.message});
+        }, function (Error) {
+            res.render('response', {path: req.path, code: Error.code, message: Error.message});
+        });
+});
 
 /*
  * 계좌정보를 확인합니다.
