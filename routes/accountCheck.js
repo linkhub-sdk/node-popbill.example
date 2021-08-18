1982b21084bfd90526fb98d4007b9b14c16aae94
ProWON <pro_won45@naver.com>
2021-08-18 10:12:33 +0900

    added comment url

--- routes/accountCheck.js
+++ routes/accountCheck.js
@@ -45,6 +45,7 @@ router.get('/', function (req, res, next) {
 
 /*
  * 1건의 계좌에 대한 예금주성명을 조회합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#CheckAccountInfo
  */
 router.get('/checkAccountInfo', function (req, res, next) {
 
@@ -71,6 +72,7 @@ router.get('/checkAccountInfo', function (req, res, next) {
 /*
  * 연동회원의 잔여포인트를 확인합니다.
  * - 과금방식이 파트너과금인 경우 파트너 잔여포인트(GetPartnerBalance API)를 통해 확인하시기 바랍니다.
+ * - https://docs.popbill.com/accountcheck/node/api#GetBalance
  */
 router.get('/getBalance', function (req, res, next) {
 
@@ -88,6 +90,7 @@ router.get('/getBalance', function (req, res, next) {
 /*
  * 연동회원 포인트 충전을 위한 페이지의 팝업 URL을 반환합니다.
  * - 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#GetChargeURL
  */
 router.get('/getChargeURL', function (req, res, next) {
 
@@ -108,6 +111,7 @@ router.get('/getChargeURL', function (req, res, next) {
 /*
  * 파트너의 잔여포인트를 확인합니다.
  * - 과금방식이 연동과금인 경우 연동회원 잔여포인트(GetBalance API)를 이용하시기 바랍니다.
+ * - https://docs.popbill.com/accountcheck/node/api#GetPartnerBalance
  */
 router.get('/getPartnerBalance', function (req, res, next) {
 
@@ -125,6 +129,7 @@ router.get('/getPartnerBalance', function (req, res, next) {
 /*
  * 파트너 포인트 충전을 위한 페이지의 팝업 URL을 반환합니다.
  * - 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#GetPartnerURL
  */
 router.get('/getPartnerURL', function (req, res, next) {
 
@@ -144,6 +149,7 @@ router.get('/getPartnerURL', function (req, res, next) {
 
 /*
  * 예금주조회 단가를 확인합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#GetUnitCost
  */
 router.get('/getUnitCost', function (req, res, next) {
 
@@ -160,6 +166,7 @@ router.get('/getUnitCost', function (req, res, next) {
 
 /*
  * 예금주조회 API 서비스 과금정보를 확인합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#GetChargeInfo
  */
 router.get('/getChargeInfo', function (req, res, next) {
 
@@ -176,6 +183,7 @@ router.get('/getChargeInfo', function (req, res, next) {
 
 /*
  * 사업자번호를 조회하여 연동회원 가입여부를 확인합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#CheckIsMember
  */
 router.get('/checkIsMember', function (req, res, next) {
 
@@ -192,6 +200,7 @@ router.get('/checkIsMember', function (req, res, next) {
 
 /*
  * 사용하고자 하는 아이디의 중복여부를 확인합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#CheckID
  */
 router.get('/checkID', function (req, res, next) {
 
@@ -208,6 +217,7 @@ router.get('/checkID', function (req, res, next) {
 
 /*
  * 사용자를 연동회원으로 가입처리합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#JoinMember
  */
 router.get('/joinMember', function (req, res, next) {
 
@@ -265,6 +275,7 @@ router.get('/joinMember', function (req, res, next) {
 
 /*
  * 연동회원 사업자번호에 담당자(팝빌 로그인 계정)를 추가합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#RegistContact
  */
 router.get('/registContact', function (req, res, next) {
 
@@ -310,6 +321,7 @@ router.get('/registContact', function (req, res, next) {
 
 /*
  * 연동회원 사업자번호에 등록된 담당자(팝빌 로그인 계정) 목록을 확인합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#ListContact
  */
 router.get('/listContact', function (req, res, next) {
 
@@ -326,6 +338,7 @@ router.get('/listContact', function (req, res, next) {
 
 /*
  * 연동회원 사업자번호에 등록된 담당자(팝빌 로그인 계정) 정보를 수정합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#UpdateContact
  */
 router.get('/updateContact', function (req, res, next) {
 
@@ -372,6 +385,7 @@ router.get('/updateContact', function (req, res, next) {
 
 /*
  * 연동회원의 회사정보를 확인합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#GetCorpInfo
  */
 router.get('/getCorpInfo', function (req, res, next) {
 
@@ -388,6 +402,7 @@ router.get('/getCorpInfo', function (req, res, next) {
 
 /*
  * 연동회원의 회사정보를 수정합니다
+ * - https://docs.popbill.com/accountcheck/node/api#UpdateCorpInfo
  */
 router.get('/updateCorpInfo', function (req, res, next) {
 
@@ -425,6 +440,7 @@ router.get('/updateCorpInfo', function (req, res, next) {
 /*
  * 팝빌 사이트에 로그인 상태로 접근할 수 있는 페이지의 팝업 URL을 반환합니다.
  * - 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
+ * - https://docs.popbill.com/accountcheck/node/api#GetAccessURL
  */
 router.get('/getAccessURL', function (req, res, next) {
 
