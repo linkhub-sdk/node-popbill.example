0a20cb5ccf637bab4457645b61c36b6fc10a83cf
ProWON <pro_won45@naver.com>
2021-08-18 18:14:12 +0900

    added Taxinvoice GetSendToNTSConfig

--- routes/taxinvoice.js
+++ routes/taxinvoice.js
@@ -2396,6 +2396,26 @@ router.get('/updateEmailConfig', function (req, res, next) {
 });
 
 /*
+ * 연동회원의 국세청 전송 옵션 설정 상태를 확인합니다.
+ * - 국세청 전송 옵션 설정은 팝빌 사이트 [전자세금계산서] > [환경설정] > [세금계산서 관리] 메뉴에서 설정할 수 있으며, API로 설정은 불가능 합니다.
+ * - https://docs.popbill.com/taxinvoice/dotnet/api#GetSendToNTSConfig
+ */
+router.get('/getSendToNTSConfig', function (req, res, next) {
+
+    // 팝빌회원 사업자번호, '-' 제외 10자리
+    var testCorpNum = '1234567890';
+
+    // 팝빌회원 아이디
+    var testUserID = 'testkorea';
+    taxinvoiceService.getSendToNTSConfig(testCorpNum,
+        function (result) {
+            res.render('Taxinvoice/SendToNTSConfig', {path: req.path, sendToNTSConfig: result});
+        }, function (Error) {
+            res.render('response', {path: req.path, code: Error.code, message: Error.message});
+        });
+});
+
+/*
  * 전자세금계산서 발행에 필요한 인증서를 팝빌 인증서버에 등록하기 위한 페이지의 팝업 URL을 반환합니다.
  * - 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
  * - 인증서 갱신/재발급/비밀번호 변경한 경우, 변경된 인증서를 팝빌 인증서버에 재등록 해야합니다.
