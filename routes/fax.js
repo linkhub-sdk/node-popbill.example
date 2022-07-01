11279df1f8251b124d897c7d7ec0c70ca394e4a8
ZIZON97 <wlgjs101@gmail.com>
2022-07-01 11:52:43 +0900

    added CheckSenderNumber API.

--- routes/fax.js
+++ routes/fax.js
@@ -44,6 +44,36 @@ router.get('/', function(req, res, next) {
 });
 
 /*
+ * 팩스 발신번호 등록여부를 확인합니다.
+ * - 발신번호 상태가 '승인'인 경우에만 리턴값 'Response'의 변수 'code'가 1로 반환됩니다.
+ * - https://docs.popbill.com/fax/node/api#CheckSenderNumber
+ */
+router.get('/checkSenderNumber', function(req, res, next) {
+
+    // 팝빌회원 사업자번호
+    var testCorpNum = '1234567890';
+
+    // 확인할 발신번호
+    var senderNumber = '';
+
+    faxService.checkSenderNumber(testCorpNum, senderNumber,
+        function(result) {
+            res.render('response', {
+                path: req.path,
+                code: result.code,
+                message: result.message
+            });
+        },
+        function(Error) {
+            res.render('response', {
+                path: req.path,
+                code: Error.code,
+                message: Error.message
+            });
+        });
+});
+
+/*
  * 발신번호를 등록하고 내역을 확인하는 팩스 발신번호 관리 페이지 팝업 URL을 반환합니다.
  * - 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
  * - https://docs.popbill.com/fax/node/api#GetSenderNumberMgtURL
