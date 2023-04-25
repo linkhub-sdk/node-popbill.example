1973558e917ec1fa8325343ce795f33554f9d97d
AlBan93 <albaneo0724@gmail.com>
2023-04-25 14:30:31 +0900

    사업자 번호/user id 추가

--- config/popbill.js
+++ config/popbill.js
@@ -10,33 +10,32 @@
  * 21번째 , 24번째 라인에 선언된 링크아이디(LinkID)와 비밀키(SecretKey)를
  * 링크허브 가입시 메일로 발급받은 인증정보를 참조하여 변경합니다.
  */
-var popbill = require('popbill');
+var popbill = require("popbill");
 
 /*
  * 팝빌 서비스 연동환경 초기화
  */
 popbill.config({
+  // 링크아이디
+  LinkID: "TESTER",
 
-    // 링크아이디
-    LinkID: 'TESTER',
+  // 비밀키
+  SecretKey: "SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I=",
 
-    // 비밀키
-    SecretKey: 'SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I=',
+  // 연동환경 설정값, 개발용(true), 상업용(false)
+  IsTest: true,
 
-    // 연동환경 설정값, 개발용(true), 상업용(false)
-    IsTest: true,
+  // 인증토큰 IP제한기능 사용여부, 권장(true)
+  IPRestrictOnOff: true,
 
-    // 인증토큰 IP제한기능 사용여부, 권장(true)
-    IPRestrictOnOff: true,
+  // 팝빌 API 서비스 고정 IP 사용여부, 기본값(false)
+  UseStaticIP: false,
 
-    // 팝빌 API 서비스 고정 IP 사용여부, 기본값(false)
-    UseStaticIP: false,
+  // 로컬서버 시간 사용 여부 true(기본값) - 사용, false(미사용)
+  UseLocalTimeYN: true,
 
-    // 로컬서버 시간 사용 여부 true(기본값) - 사용, false(미사용)
-    UseLocalTimeYN: true,
-
-    // function 타입이 아닌 error 를 파라미터로 넣고 API 요청에 대한 응답이 실패할 경우 동작하게 되는 handler.
-    defaultErrorHandler: function(Error) {
-        console.log('Error Occur : [' + Error.code + '] ' + Error.message);
-    }
+  // function 타입이 아닌 error 를 파라미터로 넣고 API 요청에 대한 응답이 실패할 경우 동작하게 되는 handler.
+  defaultErrorHandler: function (Error) {
+    console.log("Error Occur : [" + Error.code + "] " + Error.message);
+  },
 });
