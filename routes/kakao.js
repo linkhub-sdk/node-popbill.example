d92c4a678ab479a591a595307f804b93fb3408d6
redbean88 <doctortor88@gmail.com>
2024-11-06 11:16:58 +0900

    MOD : 주석 개선 외 4건

--- routes/kakao.js
+++ routes/kakao.js
@@ -6,16 +6,21 @@
  * 연동 기술지원 이메일 : code@linkhubcorp.com
  *
  * <테스트 연동개발 준비사항>
- *  1) 발신번호 사전등록을 합니다. (등록방법은 사이트/API 두가지 방식이 있습니다.)
- *    - 1. 팝빌 사이트 로그인 > [문자/팩스] > [카카오톡] > [발신번호 사전등록] 메뉴에서 등록
- *    - 2. getSenderNumberMgtURL API를 통해 반환된 URL을 이용하여 발신번호 등록
- *  2) 비즈니스 채널 등록 및 알림톡 템플릿을 신청합니다.
+ * 1) API Key 변경 (연동신청 시 메일로 전달된 정보)
+ *     - LinkID : 링크허브에서 발급한 링크아이디
+ *     - SecretKey : 링크허브에서 발급한 비밀키
+ * 2) SDK 환경설정 옵션 설정
+ *     - IsTest : 연동환경 설정, true-테스트, false-운영(Production), (기본값:true)
+ *     - IPRestrictOnOff : 인증토큰 IP 검증 설정, true-사용, false-미사용, (기본값:true)
+ *     - UseStaticIP : 통신 IP 고정, true-사용, false-미사용, (기본값:false)
+ *     - UseLocalTimeYN : 로컬시스템 시간 사용여부, true-사용, false-미사용, (기본값:true)
+ * 3) 비즈니스 채널 등록 및 알림톡 템플릿을 신청합니다.
  *    - 1. 비즈니스 채널 등록 (등록방법은 사이트/API 두가지 방식이 있습니다.)
- *       └ 팝빌 사이트 로그인 [문자/팩스] > [카카오톡] > [카카오톡 관리] > '카카오톡 채널 관리' 메뉴에서 등록
- *       └ GetPlusFriendMgtURL API 를 통해 반환된 URL을 이용하여 등록
+ *        └ 팝빌 사이트 로그인 [문자/팩스] > [카카오톡] > [카카오톡 관리] > '카카오톡 채널 관리' 메뉴에서 등록
+ *        └ GetPlusFriendMgtURL API 를 통해 반환된 URL을 이용하여 등록
  *    - 2. 알림톡 템플릿 신청 (등록방법은 사이트/API 두가지 방식이 있습니다.)
- *       └ 팝빌 사이트 로그인 [문자/팩스] > [카카오톡] > [카카오톡 관리] > '알림톡 템플릿 관리' 메뉴에서 등록
- *       └ GetATSTemplateMgtURL API 를 통해 URL을 이용하여 등록
+ *        └ 팝빌 사이트 로그인 [문자/팩스] > [카카오톡] > [카카오톡 관리] > '알림톡 템플릿 관리' 메뉴에서 등록
+ *        └ GetATSTemplateMgtURL API 를 통해 URL을 이용하여 등록.
  */
 var express = require("express");
 var router = express.Router();
@@ -1421,10 +1426,9 @@ router.get("/Search", function (req, res, next) {
     // - 미입력 시 전체조회
     var reserveYN = "";
 
-    // 사용자권한별 조회 (true / false 중 택 1)
-    // └ false = 접수한 카카오톡 전체 조회 (관리자권한)
-    // └ true = 해당 담당자 계정으로 접수한 카카오톡만 조회 (개인권한)
-    // - 미입력시 기본값 false 처리
+    // 사용자 권한별 조회
+    // └ true = 팝빌회원 아이디(UserID)로 전송한 카카오톡만 조회
+    // └ false = 전송한 카카오톡 전체 조회 : 기본값
     var senderYN = true;
 
     // 목록 페이지번호 (기본값 1)
